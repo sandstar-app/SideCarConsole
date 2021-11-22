b93f6d8ad950e87da8c7c52afc21267335124af3
Wu Nan <geek5nan@gmail.com>
2021-11-22 12:51:04 +0800

    Init Repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Form, Switch} from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [input, setInput] = React.useState(true)
+
+    return (
+        <Form
+            labelCol={{span: 8}}
+            autoComplete="off">
+            <Form.Item
+                label="远程 ABD 状态">
+                <Switch checked={input}
+                        onChange={() => {
+                            setInput(!input)
+                        }}/>
+            </Form.Item>
+            <Form.Item
+                label="开关">
+                <Button type="primary" onClick={() => {
+                    console.log("click me")
+                }}>Button</Button>
+            </Form.Item>
+            <Form.Item
+                label="日志信息">
+
+            </Form.Item>
+        </Form>
+    );
 }
 
 export default App;
